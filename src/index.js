35e800a59482d5617f73c57dabeca06623368455
Sakis T. <106918030+AthTop@users.noreply.github.com>
2024-10-24 18:11:19 +0300

    Add basic dropdown menu functonality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-import './style.css';
+import './style.css'
+
+// Select all elements with the class 'menu'
+const dropdownMenu = document.querySelectorAll('.menu')
+
+// Create a button with an eventlistener and attach it to the menus
+dropdownMenu.forEach((menu) => {
+    const menuToggle = document.createElement('button')
+    menuToggle.type = 'button'
+    menuToggle.textContent = 'Toggle'
+    menuToggle.classList.add('toggle-button')
+    menuToggle.addEventListener('click', (e) =>
+        triggerMenu(e.target.nextElementSibling)
+    )
+    menu.parentNode.prepend(menuToggle)
+})
+
+// Function to add 'visible' class to a node element
+function triggerMenu(menuNode) {
+    menuNode.classList.toggle('hidden')
+}
